915d467c02a7511fcae1d29538f6026e989c6998
nickgsuperstar <nickgsuperstar@7505edc6-dd2a-0410-a18c-bbadbf1a0011>
2007-04-06 04:04:39 +0000

    Release maker

--- trunk/makerelease.sh
+++ trunk/makerelease.sh
@@ -1,17 +1,15 @@
-#!/bin/bash
-VERSION=3.0.0
-rm -rf tmp
-mkdir tmp
-cd tmp
-svn co https://wush.net/svn/dots101/base64 modp-base64-v${VERSION}
-find . -name '.svn' | xargs rm -rf
+#!/bin/sh
+FILE=stringencoders-v3.0.0
+rm -rf $FILE
+mkdir $FILE
+cp . $FILE
 
-rm -f modp-base64-v${VERSION}/index.html
-rm -f modp-base64-v${VERSION}/performance-data.html
-rm -f modp-base64-v${VERSION}/Doxyfile
-rm -f modp-base64-v${VERSION}/makerelease.sh
-rm -rf modp-base64-v${VERSION}/doxy
+find $FILE -name '.svn' | xargs rm -rf
 
-tar -cjvf modp-base64-v${VERSION}.tar.bz2 modp-base64-v${VERSION}
-tar -czvf modp-base64-v${VERSION}.tar.gz modp-base64-v${VERSION}
+rm -f ${FILE}/index.html
+rm -f ${FILE}/Doxyfile
+rm -f ${FILE}/makerelease.sh
+rm -rf ${FILE}/doxy
+
+tar -czvf ${FILE}.tar.gz ${FILE}
 
