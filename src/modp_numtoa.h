fe72835e2717107cc32a3dc5aaad5ffc298289d3
nickgsuperstar <nickgsuperstar@7505edc6-dd2a-0410-a18c-bbadbf1a0011>
2007-04-23 00:29:33 +0000

    Fix doxygen warning

--- src/modp_numtoa.h
+++ src/modp_numtoa.h
@@ -29,16 +29,16 @@
 /** \brief convert an signed integer to char buffer
  *
  * \param[in] value
- * \param[out] output buffer.  Should be 16 chars or more.
+ * \param[out] buf the output buffer.  Should be 16 chars or more.
  */
-void modp_itoa10(int32_t value, char* str);
+void modp_itoa10(int32_t value, char* buf);
 
 /** \brief convert an unsigned integer to char buffer
  *
  * \param[in] value
- * \param[out] output buffer.  Should be 16 chars or more.
+ * \param[out] buf The output buffer, should be 16 chars or more.
  */
-void modp_uitoa10(uint32_t value, char* str);
+void modp_uitoa10(uint32_t value, char* buf);
 
 /** \brief convert a floating point number to char buffer with fixed-precision format
  *
@@ -46,10 +46,10 @@ void modp_uitoa10(uint32_t value, char* str);
  * will be switched exponential format.
  *
  * \param[in] value
- * \param[out] output buffer.  Should be 32 chars or more.
- * \param[in] precision.  Number of digits to the right of the decimal point.
+ * \param[out] buf  The allocated output buffer.  Should be 32 chars or more.
+ * \param[in] precision  Number of digits to the right of the decimal point.
  *    Can only be 0-9.
  */
-void modp_dtoa(double value, char* str, int prec);
+void modp_dtoa(double value, char* buf, int precision);
 
 #endif
