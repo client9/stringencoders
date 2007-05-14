c80bbb48043d49e6ff99c0bf1a38482a1cd245bf
nickgsuperstar <nickgsuperstar@7505edc6-dd2a-0410-a18c-bbadbf1a0011>
2007-05-14 00:15:43 +0000

    Attempt at hyper-speedup

--- src/modp_ascii.c
+++ src/modp_ascii.c
@@ -42,7 +42,9 @@
  * </PRE>
  */
 
+#include <stdint.h>
 #include "modp_ascii.h"
+#include "modp_ascii_data.h"
 void modp_toupper(char* str, int len)
 {
 	int i = len;
@@ -54,11 +56,42 @@ void modp_toupper(char* str, int len)
 
 void modp_toupper_copy(char* dest, const char* str, int len)
 {
-	int i = len;
+
+#if 1
+    int i;
 	for (i = 0; i < len; ++i) {
 		char c = str[i];
 		*dest++ = (c >= 'a' && c <= 'z') ? c - 32 : c;
 	}
+#else
+    int i;
+    const int leftover = len % 4;
+    const int imax = len - leftover;
+    const uint8_t* s = (const uint8_t*) str;
+    for (i = 0; i != imax ; i+=4) {
+        // it's important to make these variables
+        // it helps the optimizer to figure out what to do
+        uint8_t c1 = s[i], c2=s[i+1], c3=s[i+2], c4=s[i+3];
+        dest[0] = gsToUpperMap[c1];
+        dest[1] = gsToUpperMap[c2];
+        dest[2] = gsToUpperMap[c3];
+        dest[3] = gsToUpperMap[c4];
+        dest += 4;
+    }
+    switch (leftover) {
+    case 0:
+        *dest = '\0';
+        return;
+    case 3:
+        *dest++ = gsToUpperMap[i++];
+    case 2:
+        *dest++ = gsToUpperMap[i++];
+    case 1:
+        *dest++ = gsToUpperMap[i];
+        *dest = '\0';
+    }
+    return;
+#endif
 }
 
 void modp_tolower(char* str, int len)
