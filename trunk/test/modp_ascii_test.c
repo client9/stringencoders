e7a0cf29f74a2cb903f921bdd842ffca6891ce6a
nickgsuperstar <nickgsuperstar@7505edc6-dd2a-0410-a18c-bbadbf1a0011>
2007-05-18 04:59:58 +0000

    Fix tests for better coverage

--- trunk/test/modp_ascii_test.c
+++ trunk/test/modp_ascii_test.c
@@ -8,29 +8,83 @@
 #include <string.h>
 #include "minunit.h"
 
-static char* testSimple()
+static char* testLowerSimple()
 {
-    const char* case4 = "ABCD1234";
-    const char* case3 = "ABCD123";
-    const char* case2 = "ABCD12";
-    const char* case1 = "ABCD1";
+    const char* case4 = "ABCD abcd1234";
+    const char* case3 = "ABCD abcd123";
+    const char* case2 = "ABCD abcd12";
+    const char* case1 = "ABCD abcd1";
 
     char orig[100];
     strcpy(orig, case4);
     modp_tolower(orig, strlen(orig));
-    mu_assert_str_equals("abcd1234", orig);
+    mu_assert_str_equals("abcd abcd1234", orig);
 
     strcpy(orig, case3);
     modp_tolower(orig, strlen(orig));
-    mu_assert_str_equals("abcd123", orig);
+    mu_assert_str_equals("abcd abcd123", orig);
 
     strcpy(orig, case2);
     modp_tolower(orig, strlen(orig));
-    mu_assert_str_equals("abcd12", orig);
+    mu_assert_str_equals("abcd abcd12", orig);
 
     strcpy(orig, case1);
     modp_tolower(orig, strlen(orig));
-    mu_assert_str_equals("abcd1", orig);
+    mu_assert_str_equals("abcd abcd1", orig);
+
+    return 0;
+}
+
+static char* testUpperSimple()
+{
+    const char* case4 = "ABCD abcd1234";
+    const char* case3 = "ABCD abcd123";
+    const char* case2 = "ABCD abcd12";
+    const char* case1 = "ABCD abcd1";
+
+    char orig[100];
+    strcpy(orig, case4);
+    modp_toupper(orig, strlen(orig));
+    mu_assert_str_equals("ABCD ABCD1234", orig);
+
+    strcpy(orig, case3);
+    modp_toupper(orig, strlen(orig));
+    mu_assert_str_equals("ABCD ABCD123", orig);
+
+    strcpy(orig, case2);
+    modp_toupper(orig, strlen(orig));
+    mu_assert_str_equals("ABCD ABCD12", orig);
+
+    strcpy(orig, case1);
+    modp_toupper(orig, strlen(orig));
+    mu_assert_str_equals("ABCD ABCD1", orig);
+
+    return 0;
+}
+
+static char* testPrintSimple()
+{
+    const char* case4 = "ABCD abcd\t\t\t\t";
+    const char* case3 = "ABCD abcd\t\t\t";
+    const char* case2 = "ABCD abcd\t\t";
+    const char* case1 = "ABCD abcd\t";
+
+    char orig[100];
+    strcpy(orig, case4);
+    modp_toprint(orig, strlen(orig));
+    mu_assert_str_equals("ABCD abcd????", orig);
+
+    strcpy(orig, case3);
+    modp_toprint(orig, strlen(orig));
+    mu_assert_str_equals("ABCD abcd???", orig);
+
+    strcpy(orig, case2);
+    modp_toprint(orig, strlen(orig));
+    mu_assert_str_equals("ABCD abcd??", orig);
+
+    strcpy(orig, case1);
+    modp_toprint(orig, strlen(orig));
+    mu_assert_str_equals("ABCD abcd?", orig);
 
     return 0;
 }
@@ -156,7 +210,9 @@ static char* testToLowerCopy()
 }
 
 static char* all_tests() {
-    mu_run_test(testSimple);
+    mu_run_test(testUpperSimple);
+    mu_run_test(testLowerSimple);
+    mu_run_test(testPrintSimple);
     mu_run_test(testToUpperCopy);
     mu_run_test(testToLowerCopy);
     mu_run_test(testToUpper);
