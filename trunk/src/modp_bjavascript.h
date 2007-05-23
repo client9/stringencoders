80226c4a9a504f11cba33228374823c63a6870c2
nickgsuperstar <nickgsuperstar@7505edc6-dd2a-0410-a18c-bbadbf1a0011>
2007-05-23 19:30:01 +0000

    cleanup

--- trunk/src/modp_bjavascript.h
+++ trunk/src/modp_bjavascript.h
@@ -1,4 +1,4 @@
-/* -*- mode: c; c-basic-offset: 4; indent-tabs-mode: nil; tab-width: 4 -*- */
+/* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil; tab-width: 4 -*- */
 /* vi: set expandtab shiftwidth=4 tabstop=4: */
 
 /**
@@ -23,36 +23,36 @@
 extern "C" {
 #endif
 
-/**
- * "javascript" encode a stirng
- * This takes a c-string and does character escaping
- * so it can be put into a var js_string = '...';
- *
- * \param[out] dest output string.  Must
- * \param[in] str The input string
- * \param[in] len  The length of the input string, excluding any
- *   final null byte.
- */
-int modp_bjavascript_encode(char* dest, const char* str, int len);
+    /**
+     * "javascript" encode a stirng
+     * This takes a c-string and does character escaping
+     * so it can be put into a var js_string = '...';
+     *
+     * \param[out] dest output string.  Must
+     * \param[in] str The input string
+     * \param[in] len  The length of the input string, excluding any
+     *   final null byte.
+     */
+    int modp_bjavascript_encode(char* dest, const char* str, int len);
 
 #define modp_bjavascript_encode_len(A) (4*A + 1)
 
-/**
- * Given the exact size of output string.
- *
- * Can be used to allocate the right amount of memory for
- * modp_burl_encode.  Be sure to add 1 byte for final null.
- *
- * This is somewhat expensive since it examines every character
- *  in the input string
- *
- * \param[in] str  The input string
- * \param[in] len  THe length of the input string, excluding any
- *   final null byte (i.e. strlen(str))
- * \return the size of the output string, excluding the final
- *   null byte.
- */
-int modp_bjavascript_encode_strlen(const char* str, const int len);
+    /**
+     * Given the exact size of output string.
+     *
+     * Can be used to allocate the right amount of memory for
+     * modp_burl_encode.  Be sure to add 1 byte for final null.
+     *
+     * This is somewhat expensive since it examines every character
+     *  in the input string
+     *
+     * \param[in] str  The input string
+     * \param[in] len  THe length of the input string, excluding any
+     *   final null byte (i.e. strlen(str))
+     * \return the size of the output string, excluding the final
+     *   null byte.
+     */
+    int modp_bjavascript_encode_strlen(const char* str, const int len);
 
 #ifdef __cplusplus
 }
@@ -62,21 +62,21 @@ int modp_bjavascript_encode_strlen(const char* str, const int len);
 #include <string>
 namespace modp {
 
-	inline std::string& javascript_encode(std::string& s)
-	{
-		std::string x(modp_bjavascript_encode_len(s.size()), '\0');
+    inline std::string& javascript_encode(std::string& s)
+    {
+        std::string x(modp_bjavascript_encode_len(s.size()), '\0');
         int d = modp_bjavascript_encode(const_cast<char*>(x.data()), s.data(), s.size());
-		x.erase(d, std::string::npos);
+        x.erase(d, std::string::npos);
         s.swap(x);
-		return s;
-	}
+        return s;
+    }
 
     inline std::string javascript_encode(const std::string& s)
     {
-		std::string x(modp_bjavascript_encode_len(s.size()), '\0');
+        std::string x(modp_bjavascript_encode_len(s.size()), '\0');
         int d = modp_bjavascript_encode(const_cast<char*>(x.data()), s.data(), s.size());
-		x.erase(d, std::string::npos);
-		return x;
+        x.erase(d, std::string::npos);
+        return x;
     }
 }
 #endif
