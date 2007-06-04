5a0b6537b9b269ef0a8bbd63aaee4954dcc4daeb
nickgsuperstar <nickgsuperstar@7505edc6-dd2a-0410-a18c-bbadbf1a0011>
2007-06-04 17:52:19 +0000

    Alternate versions, tests

--- trunk/test/speedtest_ascii.c
+++ trunk/test/speedtest_ascii.c
@@ -5,9 +5,10 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdint.h>
-
+//#include "modp_ascii_data.h"
 #include <ctype.h>
 
+extern const char gsToUpperMap[256];
 /**
  * This is standard clib implementation of uppercasing a string.
  * It has an unfair advantage since it's inside the test file
@@ -19,6 +20,71 @@ void stdlib_toupper_copy(char* dest, const char* str, int len)
     for (i = 0; i < len; ++i) {
         // toupper is defined in <ctype.h>
         *dest++ = toupper(str[i]);
+    }
+}
+
+void modp_toupper_copy_a1(char* dest, const char* str, int len)
+{
+    int i=0;
+    uint32_t eax,ebx,ecx,edx;
+    const uint8_t* ustr = (const uint8_t*) str;
+    const int leftover = len % 4;
+    const int imax = len / 4;
+    const uint32_t* s = (const uint32_t*) str;
+    uint32_t* d = (uint32_t*) dest;
+    for (i = 0; i != imax; ++i) {
+        eax = s[i];
+        ebx = 0x7f7f7f7f;
+        edx = 0x7f7f7f7f;
+        ebx = ebx & eax;
+        ebx = ebx + 0x05050505;
+        ecx = eax;
+        ecx = ~ecx;
+        ebx = ebx & edx;
+        ebx = ebx + 0x1a1a1a1a;
+        ebx = ebx & ecx;
+        ebx = ebx >> 2;
+        ebx = ebx & 0x20202020;
+        eax = eax - ebx;
+        *d++ = eax;
+    }
+
+    i = imax*4;
+    dest = (char*) d;
+    switch (leftover) {
+        case 3: *dest++ = gsToUpperMap[ustr[i++]];
+        case 2: *dest++ = gsToUpperMap[ustr[i++]];
+        case 1: *dest++ = gsToUpperMap[ustr[i]];
+        case 0: *dest = '\0';
+    }
+}
+
+void modp_toupper_copy_a2(char* dest, const char* str, int len)
+{
+    int i=0;
+    uint32_t eax,ebx,ecx;
+    const uint8_t* ustr = (const uint8_t*) str;
+    const int leftover = len % 4;
+    const int imax = len / 4;
+    const uint32_t* s = (const uint32_t*) str;
+    uint32_t* d = (uint32_t*) dest;
+    for (i = 0; i != imax; ++i) {
+        eax = s[i];
+        ecx = ~eax;
+        ebx = (0x7f7f7f7ful & eax) + 0x05050505ul;
+        ebx = (0x7f7f7f7ful & ebx) + 0x1a1a1a1aul;
+        ebx = ((ebx & ecx) >> 2 ) & 0x20202020ul;
+        eax = eax - ebx;
+        *d++ = eax;
+    }
+
+    i = imax*4;
+    dest = (char*) d;
+    switch (leftover) {
+        case 3: *dest++ = gsToUpperMap[ustr[i++]];
+        case 2: *dest++ = gsToUpperMap[ustr[i++]];
+        case 1: *dest++ = gsToUpperMap[ustr[i]];
+        case 0: *dest = '\0';
     }
 }
 
@@ -49,6 +115,28 @@ int main()
     }
     t1 = clock();
     last = t1 -t0;
+    printf("%lu\t", (t1-t0));
+    fflush(stdout);
+
+    /**
+     ** MODP
+     **/
+    t0 = clock();
+    for (i = 0; i < max; ++i) {
+        modp_toupper_copy_a1(obuf, buf, sizeof(buf));
+    }
+    t1 = clock();
+    printf("%lu\t", (t1-t0));
+    fflush(stdout);
+
+    /**
+     ** MODP
+     **/
+    t0 = clock();
+    for (i = 0; i < max; ++i) {
+        modp_toupper_copy_a2(obuf, buf, sizeof(buf));
+    }
+    t1 = clock();
     printf("%lu\t", (t1-t0));
     fflush(stdout);
 
