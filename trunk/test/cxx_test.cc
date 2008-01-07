7c56b4ca7dcb4cb08080cd075c3f98cc25747d80
nickgsuperstar <nickgsuperstar@7505edc6-dd2a-0410-a18c-bbadbf1a0011>
2008-01-07 01:12:49 +0000

    Arg, missed one more inlcude <cstdlib> for c++

--- trunk/test/cxx_test.cc
+++ trunk/test/cxx_test.cc
@@ -1,6 +1,7 @@
 /* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil; tab-width: 4 -*- */
 /* vi: set expandtab shiftwidth=4 tabstop=4: */
 
+#include <cstdlib>
 #include <string>
 using std::string;
 
