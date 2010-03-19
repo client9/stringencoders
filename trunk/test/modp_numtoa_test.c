7438697a415126fbae99eaded381cadbc68a1fbe
nickgsuperstar <nickgsuperstar@7505edc6-dd2a-0410-a18c-bbadbf1a0011>
2010-03-19 14:00:12 +0000

    Issue 9: dtoa2, same as dtoa2 but with no trailing zeros after the decimal point

--- trunk/test/modp_numtoa_test.c
+++ trunk/test/modp_numtoa_test.c
@@ -175,6 +175,129 @@ static char* testDoubleToA()
     return 0;
 }
 
+// Helper function
+//  Removes trailing zeros
+// this is horible but it's just for testing.
+static void stripTrailingZeros(char* buf)
+{
+    size_t i;
+    int hasdot = 0;
+    for (i = 0; i < strlen(buf); ++i) {
+        if (buf[i] == '.') {
+            hasdot = 1;
+            break;
+        }
+    }
+
+    // it's just an integer
+    if (!hasdot) {
+        return;
+    }
+
+    i = strlen(buf);
+    if (i == 0) {
+        return;
+    }
+    --i;
+
+    while (i > 0 && (buf[i] == '0' || buf[i] == '.')) {
+        if (buf[i] == '.') {
+            buf[i] = '\0';
+            break;
+        } else {
+            buf[i] = '\0';
+            --i;
+        }
+    }
+}
+
+static char* testDoubleToA2()
+{
+    char buf1[100];
+    char buf2[100];
+    char msg[200];
+    double d;
+
+    /* test each combination of whole number + fraction,
+       at every precision */
+    /* and test negative version */
+    double wholes[] = {0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,
+                       67.0,101.0, 10000, 99999};
+    double frac[] = {0.0, 0.1, 0.2, 0.3, 0.4, 0.49, 0.5, 0.51, 0.6, 0.7,
+                     0.9, 0.01, 0.25, 0.125, 0.05, 0.005, 0.0005, 0.00005,
+                     0.001, 0.00001, 0.99, 0.999, 0.9999};
+    const char* formats[] = {"%.0f", "%.1f", "%.2f", "%.3f", "%.4f", "%.5f",
+                             "%.6f", "%.7f", "%.8f", "%.9f"};
+
+    int imax = sizeof(wholes)/sizeof(double);
+    int jmax = sizeof(frac)/sizeof(double);
+    int kmax = sizeof(formats)/sizeof(const char*);
+
+    int i,j,k;
+    for (i = 0; i < imax; ++i) {
+        for (j = 0; j < jmax; ++j) {
+            for (k = 0; k < kmax; ++k) {
+                d = wholes[i] + frac[j];
+
+                sprintf(msg, "whole=%f, frac=%f, prec=%d -- ",
+                        wholes[i], frac[j], k);
+
+                sprintf(buf1, formats[k], d);
+                stripTrailingZeros(buf1);
+                modp_dtoa2(d, buf2, k);
+                mu_assert_str_equals_msg(msg,buf1, buf2);
+
+                if (d != 0) {
+                    sprintf(msg, "whole=%f, frac=%f, prec=%d -- ",
+                            -wholes[i], frac[j], k);
+                    /* not dealing with "-0" issues */
+                    d = -d;
+                    sprintf(buf1, formats[k], d);
+                    stripTrailingZeros(buf1);
+
+                    modp_dtoa2(d, buf2, k);
+                    mu_assert_str_equals_msg(msg,buf1, buf2);
+                }
+
+            }
+        }
+    }
+
+    /* test very large positive number */
+    d = 1.0e200;
+    modp_dtoa2(d, buf2, 6);
+    mu_assert_str_equals("1.000000e+200", buf2);
+
+    /* test very large negative number */
+    d = -1.0e200;
+    modp_dtoa2(d, buf2, 6);
+    mu_assert_str_equals("-1.000000e+200", buf2);
+
+    /* test very small positive number */
+    d = 1e-10;
+    sprintf(buf1, "%.6f", d);
+    stripTrailingZeros(buf1);
+
+    modp_dtoa2(d, buf2, 6);
+    mu_assert_str_equals(buf1, buf2);
+
+    /* test very small negative number */
+    d = -1e-10;
+    sprintf(buf1, "%.6f", d);
+    stripTrailingZeros(buf1);
+
+    modp_dtoa2(d, buf2, 6);
+    mu_assert_str_equals(buf1, buf2);
+
+    // test bad precision values
+    d = 1.1;
+    modp_dtoa(d, buf2, -1);
+    mu_assert_str_equals("1", buf2);
+    modp_dtoa2(d, buf2, 10);
+    mu_assert_str_equals("1.1", buf2);
+    return 0;
+}
+
 /* From Issue 7  -- http://code.google.com/p/stringencoders/issues/detail?id=7
  * thanks to http://code.google.com/u/simhasana/
  */
@@ -195,7 +318,6 @@ static char* testOverflowLITOA() {
     return 0;
 }
 
-
 static char* testOverflowITOA() {
     char buf1[100];
     char buf2[100];
@@ -213,13 +335,13 @@ static char* testOverflowITOA() {
     return 0;
 }
 
-
 static char* all_tests() {
     mu_run_test(testITOA);
     mu_run_test(testUITOA);
     mu_run_test(testLITOA);
     mu_run_test(testULITOA);
     mu_run_test(testDoubleToA);
+    mu_run_test(testDoubleToA2);
     mu_run_test(testOverflowLITOA);
     mu_run_test(testOverflowITOA);
     return 0;
