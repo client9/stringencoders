2bd339aa66690d03c5d09a9ef8510b11a41e1d5e
nickgsuperstar <nickgsuperstar@7505edc6-dd2a-0410-a18c-bbadbf1a0011>
2011-01-03 01:01:34 +0000

    fix _int_equals_msg to actually display the error message

--- test/minunit.h
+++ test/minunit.h
@@ -6,13 +6,15 @@
 
 #define mu_assert(test) do { if (!(test)) { snprintf(mu_buf, sizeof(mu_buf), "ASSERTION FAILED: %s:%d", __FILE__, __LINE__); return mu_buf; } } while (0)
 
+#define mu_assert_msg(msg, test) do { if (!(test)) { snprintf(mu_buf, sizeof(mu_buf), "ASSERTION FAILED: %s %s:%d", msg, __FILE__, __LINE__); return mu_buf; } } while (0)
+
 #define mu_assert_int_equals(lhs, rhs)  do { if (lhs != rhs) { snprintf(mu_buf, sizeof(mu_buf), "ASSERTION FAILED: %s:%d", __FILE__, __LINE__); return mu_buf; } } while (0)
 
 #define mu_assert_str_equals(lhs, rhs)  do { if (strcmp(lhs,rhs) != 0) { snprintf(mu_buf, sizeof(mu_buf), "ASSERTION FAILED: %s:%d %s != %s", __FILE__, __LINE__, lhs, rhs); return mu_buf; } } while (0)
 
 #define mu_assert_str_equals_msg(msg, lhs, rhs)  do { if (strcmp(lhs,rhs) != 0) { snprintf(mu_buf, sizeof(mu_buf), "ASSERTION FAILED: %s  %s:%d %s != %s", msg, __FILE__, __LINE__, lhs, rhs); return mu_buf; } } while (0)
 
-#define mu_assert_int_equals_msg(msg, lhs, rhs)  do { if (lhs != rhs) { snprintf(mu_buf, sizeof(mu_buf), "ASSERTION FAILED: %s:%d", __FILE__, __LINE__); return mu_buf; } } while (0)
+#define mu_assert_int_equals_msg(msg, lhs, rhs)  do { if (lhs != rhs) { snprintf(mu_buf, sizeof(mu_buf), "ASSERTION FAILED: %s  %s:%d", msg, __FILE__, __LINE__); return mu_buf; } } while (0)
 
 #define mu_run_test(test) do { printf("."); fflush(stdout); char *message = test(); tests_run++; \
                                 if (message) return message; } while (0)
