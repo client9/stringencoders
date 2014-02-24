80f864f8cd726e07de272e3cc57d3b2af24c2aa9
nickgatclient9dotcom@gmail.com <nickgatclient9dotcom@gmail.com@7505edc6-dd2a-0410-a18c-bbadbf1a0011>
2014-02-24 11:48:54 +0000

    WIP messagepack encode

---
+++ trunk/src/modp_messagepack.h
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <stddef.h>
+
+typedef struct {
+  size_t size;
+  char* dest;
+} modp_msgpk_ctx;
+
+void modp_msgpk_init(modp_msgpk_ctx* ctx, char* dest);
+size_t modp_msgpk_end(modp_msgpk_ctx* ctx);
+void modp_msgpk_add_bool(modp_msgpk_ctx* ctx, int val);
+void modp_msgpk_add_double(modp_msgpk_ctx* ctx, double d);
+void modp_msgpk_add_int32(modp_msgpk_ctx* ctx, int val);
+void modp_msgpk_add_uint32(modp_msgpk_ctx* ctx, unsigned int val);
+
+void modp_msgpk_add_string(modp_msgpk_ctx* ctx, const char*, size_t);
+void modp_msgpk_add_cstring(modp_msgpk_ctx* ctx, const char*);
+void modp_msgpk_map_open(modp_msgpk_ctx* ctx, size_t count);
+
+/* Needed in JSON, not  needed in messagepack */
+void modp_msgpk_map_close(modp_msgpk_ctx* ctx) { (void)ctx; }
+
+void modp_msgpk_ary_open(modp_msgpk_ctx* ctx, size_t count);
+
+/* Needed in JSON, not  needed in messagepack */
+void modp_msgpk_ary_close(modp_msgpk_ctx* ctx) { (void)ctx; }
