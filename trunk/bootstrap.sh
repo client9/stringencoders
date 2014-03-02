3998ff8e24d9c5732896532f19aca33c71c5701d
nickgatclient9dotcom@gmail.com <nickgatclient9dotcom@gmail.com@7505edc6-dd2a-0410-a18c-bbadbf1a0011>
2014-03-02 07:01:34 +0000

    big autotools update, cleanup

--- trunk/bootstrap.sh
+++ trunk/bootstrap.sh
@@ -1,25 +1,3 @@
 #!/bin/sh
-
-#
-# script to regen the autotools
-#  glibtoolize might be "libtoolize" on your system
-#
-set -x
-aclocal
-
-# test for MacPorts version first
-#  Mac version of libtool is bizarro
-if [ "`which glibtoolize`" != "" ]; then
-   LIBTOOLIZE=glibtoolize
-elif [ "`which libtool`" != "" ]; then
-   LIBTOOLIZE=libtoolize
-else
-   echo "Perhap you need to install libtool?"
-   exit 1
-fi
-echo "Using ${LIBTOOLIZE}..."
-
-${LIBTOOLIZE} --force --copy
-autoheader
-automake --add-missing --copy
-autoconf
+autoreconf --install
+automake --add-missing >/dev/null 2>&1
