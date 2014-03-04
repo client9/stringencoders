809793d6c9f58f0088e9fbd6fae6a3d18d6b726c
nickgatclient9dotcom@gmail.com <nickgatclient9dotcom@gmail.com@7505edc6-dd2a-0410-a18c-bbadbf1a0011>
2014-03-04 04:13:06 +0000

    C90 Compatibility

--- trunk/test/modp_b16_test.c
+++ trunk/test/modp_b16_test.c
@@ -1,11 +1,16 @@
 /* -*- mode: c; c-basic-offset: 4; indent-tabs-mode: nil; tab-width: 4 -*- */
 /* vi: set expandtab shiftwidth=4 tabstop=4: */
 
+/* we compile as C90 but use snprintf */
+#define _ISOC99_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "modp_b16.h"
+
 #include "minunit.h"
+
+#include "modp_b16.h"
 
 static char* testEndian(void)
 {
