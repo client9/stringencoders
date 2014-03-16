41c3f2927581c6fc65f4fe895fdb29d1f17ab7db
nickgatclient9dotcom@gmail.com <nickgatclient9dotcom@gmail.com@7505edc6-dd2a-0410-a18c-bbadbf1a0011>
2014-03-16 08:54:08 +0000

    add inline

--- src/modp_messagepack.h
+++ src/modp_messagepack.h
@@ -37,12 +37,12 @@ void modp_msgpk_add_cstring(modp_msgpk_ctx* ctx, const char*);
 void modp_msgpk_map_open(modp_msgpk_ctx* ctx, size_t count);
 
 /* Needed in JSON, not  needed in messagepack */
-void modp_msgpk_map_close(modp_msgpk_ctx* ctx) { (void)ctx; }
+inline void modp_msgpk_map_close(modp_msgpk_ctx* ctx) { (void)ctx; }
 
 void modp_msgpk_ary_open(modp_msgpk_ctx* ctx, size_t count);
 
 /* Needed in JSON, not  needed in messagepack */
-void modp_msgpk_ary_close(modp_msgpk_ctx* ctx) { (void)ctx; }
+inline void modp_msgpk_ary_close(modp_msgpk_ctx* ctx) { (void)ctx; }
 
 MODP_C_END_DECLS
 
