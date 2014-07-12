1949eec1c8e2620892077b6bfcf55a63e6772905
nickgatclient9dotcom@gmail.com <nickgatclient9dotcom@gmail.com@7505edc6-dd2a-0410-a18c-bbadbf1a0011>
2014-07-12 05:31:29 +0000

    fix and improve json encoder

--- src/modp_json_gen.py
+++ src/modp_json_gen.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
+def intarray_to_c(ary, name):
+    parts = []
+    parts.append("static const size_t {0}[{1}] = {{".format(name,len(ary)))
+    count = 0
+    for val in ary:
+        if (count % 10 == 0):
+            parts.append("\n  ")
+        parts.append("{0}, ".format(val))
+        count += 1
+    parts.append("\n};\n")
+    return ''.join(parts)
+
 def chararray_to_c(ary, name):
     parts = []
     parts.append("static const uint8_t {0}[{1}] = {{".format(name,
@@ -8,18 +20,26 @@ def chararray_to_c(ary, name):
     for val in ary:
         if (count % 10 == 0):
             parts.append("\n ")
-        parts.append("{0:>7}".format("'" + val + "',"))
+        parts.append("{0:>6}".format("'" + val + "',"))
         count += 1
 
     parts.append("\n};\n")
     return ''.join(parts)
 
+def json_encodelen_map():
+    lenmap = [ 6 for i in range(256) ]
+    for i in range(32,128):
+        lenmap[i] = 1
+    for i in [0x08, 0x09, 0x0a,  0x0c, 0x0d, 0x5c, 0x22]:
+        lenmap[i] = 2
+    return lenmap
+
 def json_encode_map():
-    encodemap = [ "\\0" for i in range(256) ]
+    encodemap = [ 'u' for i in range(256) ]
 
     # things to quad-hex encode  \u00XY
-    for i in range(32):
-        encodemap[i] = 'A';
+    for i in range(32,128):
+        encodemap[i] = 'a';
 
     # special escapes
     encodemap[0x08] = 'b'
@@ -36,3 +56,5 @@ if __name__ == '__main__':
 
     with open('modp_json_data.h', 'w') as fd:
         fd.write(chararray_to_c(json_encode_map(), "gsJSONEncodeMap"))
+        fd.write("\n")
+        fd.write(intarray_to_c(json_encodelen_map(), "gsJSONEncodeLenMap"))
