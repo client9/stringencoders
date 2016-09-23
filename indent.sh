ebdc8e3f404e98f1370bd4bc244da43c5b281406
Nick Galbreath <nickg@signalsciences.com>
2016-09-23 22:40:15 +0000

    allow override

--- indent.sh
+++ indent.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-clang-format --version
-find . -name '*.[ch]' | xargs clang-format -i -style=WebKit
+
+if [ -z "${INDENT}" ]; then
+  INDENT=clang-format
+fi
+${INDENT} --version
+find . -name '*.[ch]' | xargs ${INDENT} -i -style=WebKit
 git status --porcelain
 
